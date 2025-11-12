c04787aa13347f44674c91422b3ed2d6894a3a63
Arthur Reis <arthur.reis@estudante.iftm.edu.br>
2025-11-12 13:50:14 -0300

    fix: Corrijindo API response

--- src/main/java/br/com/bancofeira/banco_feira/dto/UsuarioResponseDto.java
+++ src/main/java/br/com/bancofeira/banco_feira/dto/UsuarioResponseDto.java
@@ -1,8 +1,11 @@
 package br.com.bancofeira.banco_feira.dto;
 
 import lombok.Data;
+
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 public class UsuarioResponseDto {
@@ -18,7 +21,25 @@ public class UsuarioResponseDto {
         dto.setId(usuario.getId());
         dto.setNome(usuario.getNome());
         dto.setEmail(usuario.getEmail());
+
         dto.setDataCadastro(usuario.getDataCadastro());
+
+        if (usuario.getEmpresas() != null) {
+            dto.setEmpresaIds(usuario.getEmpresas().stream()
+                    .map(br.com.bancofeira.banco_feira.model.Empresa::getId)
+                    .collect(Collectors.toSet()));
+        } else {
+            dto.setEmpresaIds(new HashSet<>());
+        }
+
+        if (usuario.getRoles() != null) {
+            dto.setRoles(usuario.getRoles().stream()
+                    .map(RoleDto::fromEntity)
+                    .collect(Collectors.toSet()));
+        } else {
+            dto.setRoles(new HashSet<>());
+        }
+
         return dto;
     }
 }
